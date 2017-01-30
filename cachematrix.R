da5e64b1b917faf97325bf9af0fd0c91a8a3a44f
Michael Smith <michael.christian.smith@gmail.com>
2017-01-29 17:50:19 -0700

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##
+## Cache a maxtrix using makeCacheMatrix. Example:
+## > m <- matrix(c(1, 2, 3, 4), 2, 2)
+## > x <- makeCacheMatrix(m)
+##
+## Use the cached matrix by sending the return value
+## of makeCacheMatrix to cacheSolve. Example:
+## > cacheSolve(x)
 
 ## Write a short comment describing this function
-
+##
+## makeCacheMatrix takes a matrix and returns a vector
+## of functions that will store and retrieve both it
+## and its solution.
 makeCacheMatrix <- function(cachedMatrix = matrix()) {
 	solvedMatrix <- NULL
 	set <- function(newCachedMatrix) {
@@ -20,7 +31,10 @@ makeCacheMatrix <- function(cachedMatrix = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##
+## cacheSolve takes a makeCacheMatrix vector and returns
+## the solution to the cached matrix. It will not re-
+## compute the solution if already done.
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
 	solvedMatrix <- x$getSolvedMatrix()
